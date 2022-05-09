f5788f5c2fbec959c89788ec6a88c23e94787b68
llbyron31 <82618775+llbyron31@users.noreply.github.com>
2022-05-09 17:49:58 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+       <Ricckpages
+      v-for="({nombre, src}, $index) of personajes"
+      :key="$index"
+      :nombre="nombre"
+      :imagen="src"
+    />
+  </div>
+</template>
+
+<script>
+import Ricckpages from './components/Ricckpages'
+export default {
+  name: 'App',
+  components: {Ricckpages},
+    data: () => ({
+    personajes: [
+      {
+        nombre: "Rick Sanchez",
+        src: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+      },
+      {
+        nombre: "Morty Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+      },
+      {
+        nombre: "Summer Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+      },
+      {
+        nombre: "Beth Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+      },
+      {
+        nombre: "Jerry Smith",
+        src: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+      },
+    ],
+  }),
+}
+</script>
+
+<style>
+  body{
+    background: url('https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png');
+    margin: 0;
+    padding: 0;
+  }
+</style>
